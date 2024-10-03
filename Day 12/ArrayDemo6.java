ba46ffc2aab0cb0603a462e379de47140d8ca94d
Aashiqui <ashikmail2747@gmail.com>
2024-10-03 19:18:38 +0530

    problem 6 completed

--- Day 12/ArrayDemo6.java
+++ Day 12/ArrayDemo6.java
@@ -1,21 +1,33 @@
 public class ArrayDemo6 {
     public static void main(String[] args) {
-        int a[] = { 1, 2, 3, 1 };
+        int a[] = { 1, 2, 3, 1, 2,3 };
         int sum = 0;
         for (int i = 0; i < a.length; i++) {
             boolean isrepeat = false;
-            for(int j=0;j<a.length;j++)
-            {
-                if()
+            for (int j = 0; j <i; j++) {
+                if(a[i]==a[j])
+                {
+                    isrepeat=true;
+                    break;
+                }
             }
-           
-                
-                if (!isrepeat) {
-                    System.out.print(a[i]+" ");
-                    sum = sum + a[i];
+            if(!isrepeat){
+                boolean isrepeated=false;
+                for(int k=i+1;k<a.length;k++)
+                {
+                    if(a[i]==a[k])
+                    {
+                        isrepeated=true;
+                        break;
+                    }
+                }
+                if(!isrepeated)
+                {
+                    sum=sum+a[i];
                 }
             }
 
         }
         System.out.println(sum);
-    }}
+    }
+}
